a24028ffbe88a33c01636a720c62cd00ee2b71d5
Amanda Anderson <aanderson_120@yahoo.com>
2020-10-28 19:40:45 -0500

    structure complete

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burgers_db;
+
+INSERT INTO burgers (burgers_name) 
+VALUES ("Juicy Lucy"), ("Roadhouse Burger"), ("Montana Jack Burger"), ("The Garlic Cheese Burger"), ("Blue Cheese Burger"), ("Supreme Burger");
+
+
+select * from burgers;
