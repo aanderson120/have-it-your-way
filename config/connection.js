66594bfa6752b4980207678321feb168bd7f531d
Amanda Anderson <aanderson_120@yahoo.com>
2020-10-30 17:56:33 -0500

    update connection

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,10 @@
 var mysql = require("mysql");
+var connection;
 
 if (process.env.JAWSDB_url) {
   connection = mysql.createConnection(process.env.JAWSDB_url);
 } else {
-var connection = mysql.createConnection({
+  connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
